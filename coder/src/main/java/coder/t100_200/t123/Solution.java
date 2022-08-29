46bcf1d94bd36d701d3c8b18bab9d6623bffc7e4
gatesma <gatesma@foxmail.com>
2022-08-29 20:19:23 +0800

    123 股票

---
+++ coder/src/main/java/coder/t100_200/t123/Solution.java
@@ -0,0 +1,36 @@
+package coder.t100_200.t123;
+
+/**
+ * Solution
+ *
+ * @author by gatesma.
+ */
+public class Solution {
+
+    public static int maxProfit(int[] prices) {
+        int n = prices.length;
+        int max_k = 2;
+        int[][][] dp = new int[n][max_k + 1][2];
+
+        for (int i = 0; i < n; i++) {
+            for (int k = max_k; k >= 1; k--) {
+                if (i == 0) {
+                    dp[0][k][0] = 0;
+                    dp[0][k][1] = -prices[i];
+                    continue;
+                }
+                dp[i][k][0] = Math.max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i]);
+                dp[i][k][1] = Math.max(dp[i - 1][k][1], dp[i - 1][k - 1][0] - prices[i]);
+            }
+        }
+
+        return dp[n - 1][2][0];
+
+    }
+
+    public static void main(String[] args) {
+        // 输入：prices = [3,3,5,0,0,3,1,4]
+        // 输出：6
+        System.out.println(maxProfit(new int[]{3, 3, 5, 0, 0, 3, 1, 4}));
+    }
+}
