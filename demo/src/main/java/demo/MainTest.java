73749478b72153c06776b0a7393692fa78d52145
gatesma <gatesma@foxmail.com>
2022-10-03 09:31:08 +0800

    1

--- demo/src/main/java/demo/MainTest.java
+++ demo/src/main/java/demo/MainTest.java
@@ -11,19 +11,23 @@ public class MainTest {
 
     public static AtomicInteger num = new AtomicInteger(0);
 
-    public static void main(String[] args) throws InterruptedException {
-        Runnable runnable = () -> {
-            for (int i = 0; i < 1000000000; i++) {
-                num.getAndAdd(1);
-            }
-            System.out.println(Thread.currentThread().getName() + "执行结束!");
-        };
-        System.out.println("start");
-        Thread t1 = new Thread(runnable);
-        Thread t2 = new Thread(runnable);
-        t1.start();
-        t2.start();
-        Thread.sleep(1000);
-        System.out.println("num = " + num);
+    public static void main(String[] args) {
+        System.out.print("hello world!");
     }
+
+    // public static void main(String[] args) throws InterruptedException {
+    //     Runnable runnable = () -> {
+    //         for (int i = 0; i < 1000000000; i++) {
+    //             num.getAndAdd(1);
+    //         }
+    //         System.out.println(Thread.currentThread().getName() + "执行结束!");
+    //     };
+    //     System.out.println("start");
+    //     Thread t1 = new Thread(runnable);
+    //     Thread t2 = new Thread(runnable);
+    //     t1.start();
+    //     t2.start();
+    //     Thread.sleep(1000);
+    //     System.out.println("num = " + num);
+    // }
 }
