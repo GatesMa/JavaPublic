66c7317ae4e675594bc679a7dd48c25b6580b994
gatesma <gatesma@foxmail.com>
2022-10-04 19:41:46 +0800

    LIS

--- coder/src/main/java/coder/leetcode/editor/cn/LongestIncreasingSubsequence300.java
+++ coder/src/main/java/coder/leetcode/editor/cn/LongestIncreasingSubsequence300.java
@@ -9,35 +9,29 @@ public class LongestIncreasingSubsequence300 {
 
     public static void main(String[] args) {
         Solution solution = new LongestIncreasingSubsequence300().new Solution();
-        System.out.println(solution.lengthOfLIS(new int[]{1, 3, 6, 7, 9, 4, 10, 5, 6}));
+        System.out.println(solution.lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
 
         public int lengthOfLIS(int[] nums) {
-            if (nums == null) {
+            if (nums == null || nums.length == 0) {
                 return 0;
             }
-            if (nums.length <= 1) {
-                return nums.length;
-            }
 
-            int[] dp = new int[nums.length];
+            // dp[i] 0 - i 的最长上升子序列
+            int[] dp = new int[nums.length + 1];
             Arrays.fill(dp, 1);
-
-            int max = 1;
-            for (int i = 0; i < nums.length; i++) {
+            for (int i = 1; i < nums.length; i++) {
                 for (int j = 0; j < i; j++) {
                     if (nums[i] > nums[j]) {
                         dp[i] = Math.max(dp[i], dp[j] + 1);
-                        max = Math.max(max, dp[i]);
                     }
                 }
             }
 
-
-            return max;
+            return Arrays.stream(dp).max().getAsInt();
         }
     }
     //leetcode submit region end(Prohibit modification and deletion)
