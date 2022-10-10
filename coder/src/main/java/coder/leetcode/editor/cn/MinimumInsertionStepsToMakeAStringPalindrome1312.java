09354571bcd91ade7ece66e045b057af7d63aef8
gatesma <gatesma@foxmail.com>
2022-10-10 19:40:38 +0800

    最长公共子序列

---
+++ coder/src/main/java/coder/leetcode/editor/cn/MinimumInsertionStepsToMakeAStringPalindrome1312.java
@@ -0,0 +1,47 @@
+package coder.leetcode.editor.cn;
+
+public class MinimumInsertionStepsToMakeAStringPalindrome1312 {
+
+    // CheckStyle
+    private final String time = "2022-10-10 19:39:42";
+
+    public static void main(String[] args) {
+        Solution solution = new MinimumInsertionStepsToMakeAStringPalindrome1312().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        public int minInsertions(String s) {
+            return s.length() - longestPalindromeSubseq(s);
+        }
+
+        public int longestPalindromeSubseq(String s) {
+            int n = s.length();
+
+            // dp[i][j] s[i,j]的最长回文子串，求dp[0,n-1]
+            int[][] dp = new int[n + 1][n + 1];
+
+            // base case
+            for (int i = 0; i < n; i++) {
+                dp[i][i] = 1;
+            }
+
+            for (int i = n - 1; i >= 0; i--) {
+                for (int j = i + 1; j < n; j++) {
+                    if (s.charAt(i) == s.charAt(j)) {
+                        dp[i][j] = dp[i + 1][j - 1] + 2;
+                    } else {
+                        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+
+            return dp[0][n - 1];
+        }
+
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+
+}
---
+++ coder/src/main/java/coder/leetcode/editor/cn/doc/content/MinimumInsertionStepsToMakeAStringPalindrome1312.md
@@ -0,0 +1,55 @@
+<p>给你一个字符串&nbsp;<code>s</code>&nbsp;，每一次操作你都可以在字符串的任意位置插入任意字符。</p>
+
+<p>请你返回让&nbsp;<code>s</code>&nbsp;成为回文串的&nbsp;<strong>最少操作次数</strong>&nbsp;。</p>
+
+<p>「回文串」是正读和反读都相同的字符串。</p>
+
+<p>&nbsp;</p>
+
+<p><strong>示例 1：</strong></p>
+
+<pre>
+<strong>输入：</strong>s = "zzazz"
+<strong>输出：</strong>0
+<strong>解释：</strong>字符串 "zzazz" 已经是回文串了，所以不需要做任何插入操作。
+</pre>
+
+<p><strong>示例 2：</strong></p>
+
+<pre>
+<strong>输入：</strong>s = "mbadm"
+<strong>输出：</strong>2
+<strong>解释：</strong>字符串可变为 "mbdadbm" 或者 "mdbabdm" 。
+</pre>
+
+<p><strong>示例 3：</strong></p>
+
+<pre>
+<strong>输入：</strong>s = "leetcode"
+<strong>输出：</strong>5
+<strong>解释：</strong>插入 5 个字符后字符串变为 "leetcodocteel" 。
+</pre>
+
+<p>&nbsp;</p>
+
+<p><strong>提示：</strong></p>
+
+<ul>
+	<li><code>1 &lt;= s.length &lt;= 500</code></li>
+	<li><code>s</code>&nbsp;中所有字符都是小写字母。</li>
+</ul>
+<details><summary><strong>Related Topics</strong></summary>字符串 | 动态规划</details><br>
+
+<div>👍 158, 👎 0</div>
+
+<div id="labuladong"><hr>
+
+**通知：[数据结构精品课](https://aep.h5.xeknow.com/s/1XJHEO) 已更新到 V2.0，第 12 期刷题打卡 [开始报名](https://aep.xet.tech/s/XhcRc)，点击这里体验 [刷题全家桶](https://labuladong.gitee.io/algo/images/others/%E5%85%A8%E5%AE%B6%E6%A1%B6.jpg)。**
+
+
+
+<p><strong><a href="https://labuladong.github.io/article?qno=1312" target="_blank">⭐️labuladong 题解</a></strong></p>
+</div>
+
+
+
